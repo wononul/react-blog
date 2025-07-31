3772637e01ff606b890548da6bfb7dcbdfdcec01
BAC-ASPN <208172431+wononul@users.noreply.github.com>
2025-07-31 16:59:39 +0900

    WIP: add user register route

--- index.js
+++ index.js
@@ -7,10 +7,19 @@ const app = express(); // app이라는 새로운 Express 앱 만듦
 
 const mongoose = require('mongoose'); // MongoDB ODM 모듈 불러오기
 
+const { User } = require('./model/user');
+
 mongoose.connect(MONGO_URI)
         .then(() => console.log('DB Connected!'))
         .catch(err => console.error(err));
 // console.log("MONGO_URI:", MONGO_URI);
+
+// 회원 등록 (수정중)
+app.post('/api/user/register', (req, res) => {
+    const user = new User(req.body);
+    const userData = user.save;
+    return res.status(200).json({ success: true, user:userData});
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`) // `(backtick): ES6 표준 문법, 변수 작성 시 사용
