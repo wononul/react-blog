0c331384b2035248eab5ecbe198a7f43dd6773ff
BAC-ASPN <208172431+wononul@users.noreply.github.com>
2025-08-01 13:55:14 +0900

    feat(user-register): complete user registration API with validation and async handling
    - Add body-parser middleware to correctly parse JSON and URL-encoded data
    - Refactored /api/user/register route to use async/await syntax
    - Fix validation error caused by undefined req.body fields

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express = require('express'); // Express 모듈 사용
 const app = express(); // app이라는 새로운 Express 앱 만듦
 
 const mongoose = require('mongoose'); // MongoDB ODM 모듈 불러오기
+const bodyParser = require('body-parser'); // HTTP 요청 본문(body) 파싱하는 미들웨어 불러오기
 const { User } = require('./model/user');
 
 mongoose.connect(MONGO_URI)
@@ -13,12 +14,15 @@ mongoose.connect(MONGO_URI)
         .catch(err => console.error(err));
 // console.log("MONGO_URI:", MONGO_URI);
 
-// 회원 등록 (작업중_250801)
-app.post('/api/user/register', (req, res) => {
+app.use(bodyParser.urlencoded( { extended: true })); // application/x-www-form-urlencoded
+app.use(bodyParser.json()); // application/json
+
+// 회원 등록
+app.post('/api/user/register', async (req, res) => {
     try {
         const user = new User(req.body);
-        console.log(user);
-        const userData = user.save();
+        // console.log(user);
+        const userData = await user.save();
         return res.status(200).json({ success: true, user:userData});
     } catch {
         return res.status(400).json({ success: false, err});
